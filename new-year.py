cbae5a31f5aa29ddfd2e8ff8c404ffb7f11a2ece
Dylan Brotherston <dylanbbbrother@gmail.com>
2022-12-03 01:56:41 +1100

    init

---
+++ new-year.py
@@ -0,0 +1,41 @@
+#! /usr/bin/env python3
+
+from sys import argv, exit
+from os import mkdir, chmod, symlink
+from os.path import exists
+from string import Template
+
+if len(argv) != 2:
+    print(f"Usage: {argv[1]} <year>")
+    exit(1)
+
+try:
+    year = int(argv[1])
+except ValueError:
+    print(f"Invalid year: {argv[1]}")
+    exit(1)
+
+if not exists("template.py"):
+    print("template.py not found")
+    exit(1)
+
+template = open("template.py").read()
+
+if not exists("share.py"):
+    print("share.py not found")
+    exit(1)
+
+if not exists(f"year-{year}"):
+    print(f"Creating directory for year {year}")
+    mkdir(f"year-{year}")
+
+T = Template(template)
+
+for i in range(1, 26):
+    if not exists(f"year-{year}/day-{i}"):
+        print(f"Creating day {year}/{i}")
+
+        mkdir(f"year-{year}/day-{i}")
+        open(f"year-{year}/day-{i}/solve.py", "w").write(T.substitute({"DAY": i, "YEAR": year}))
+        chmod(f"year-{year}/day-{i}/solve.py", 0o755)
+        symlink("../../share.py", f"year-{year}/day-{i}/share.py")
---
+++ template.py
@@ -0,0 +1,36 @@
+#! /usr/bin/env python3
+
+import itertools as it
+import more_itertools as mit
+import functools as ft
+import regex as re
+import numpy as np
+import urllib3 as ul
+import dateutil.parser as dp
+
+from aocd import get_data, submit # type: ignore
+
+from share import *
+
+
+PLACEHOLDER: object = object()
+
+
+data: str = clean(get_data(year=${YEAR}, day=${DAY}))
+sample_data: str = clean("""
+""")
+
+
+def A(input: str) -> int:
+    return None
+
+
+def B(input: str) -> int:
+    return None
+
+
+assert A(sample_data) == PLACEHOLDER, A(sample_data)
+submit(A(data), part="a", day=${DAY}, year=${YEAR})
+
+assert B(sample_data) == PLACEHOLDER, B(sample_data)
+submit(B(data), part="b", day=${DAY}, year=${YEAR})

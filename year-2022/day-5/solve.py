da64feb66f52bc8ff01c7d0ec5668bcec36fbe5d
Dylan Brotherston <dylanbbbrother@gmail.com>
2022-12-05 17:56:58 +1100

    2022/5

---
+++ year-2022/day-5/solve.py
@@ -0,0 +1,76 @@
+#! /usr/bin/env python3
+
+import itertools as it
+import more_itertools as mit
+import functools as ft
+import regex as re
+import numpy as np
+import urllib3 as ul
+import collections as cl
+import dateutil.parser as dp
+
+from aocd import get_data, submit  # type: ignore
+
+from share import *
+
+
+PLACEHOLDER: object = object()
+
+
+data: str = clean(get_data(year=2022, day=5))
+sample_data: str = clean(
+    """
+    [D]
+[N] [C]
+[Z] [M] [P]
+ 1   2   3
+
+move 1 from 2 to 1
+move 3 from 1 to 3
+move 2 from 2 to 1
+move 1 from 1 to 2
+"""
+)
+
+
+def A(input: str) -> str:
+    init, moves = by_paragraphs(input)
+    stacks = cl.defaultdict(list)
+    for line in by_lines(init)[:-1]:
+        m = re.fullmatch(r"[[ ](.)[] ](?: [[ ](.)[] ])*", line)
+        for i, C in enumerate(m.captures(1) + m.captures(2), 1):
+            if C != " ":
+                stacks[i].insert(0, C)
+
+    for move in by_lines(moves):
+        m = re.fullmatch(r"move (\d+) from (\d+) to (\d+)", move)
+        num, src, dst = map(int, m.groups())
+        for _ in range(num):
+            stacks[dst].append(stacks[src].pop())
+
+    return "".join(map(lambda x: x[1][-1], sorted(stacks.items(), key=lambda x: x[0])))
+
+
+def B(input: str) -> str:
+    init, moves = by_paragraphs(input)
+    stacks = cl.defaultdict(list)
+    for line in by_lines(init)[:-1]:
+        m = re.fullmatch(r"[[ ](.)[] ](?: [[ ](.)[] ])*", line)
+        for i, C in enumerate(m.captures(1) + m.captures(2), 1):
+            if C != " ":
+                stacks[i].insert(0, C)
+
+    for move in by_lines(moves):
+        m = re.fullmatch(r"move (\d+) from (\d+) to (\d+)", move)
+        num, src, dst = map(int, m.groups())
+        stacks[dst] += stacks[src][-num:]
+        stacks[src] = stacks[src][:-num]
+
+    return "".join(map(lambda x: x[1][-1], sorted(stacks.items(), key=lambda x: x[0])))
+
+
+assert A(sample_data) == "CMZ", A(sample_data)
+submit(A(data), part="a", day=5, year=2022)
+
+assert B(sample_data) == "MCD", B(sample_data)
+submit(B(data), part="b", day=5, year=2022)
